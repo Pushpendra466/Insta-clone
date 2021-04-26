f99544adbb7ba913ec24f224d66ac6e5b502262f
Pushpendra kumar <pushpendra466@gmail.com>
2021-04-26 22:19:07 +0530

    post showing on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,22 @@
+const Post = require('../models/post');
+module.exports.index = async (req,res)=>{
+    // Post.find({},(err,posts)=>{
+    //     if(err){
+    //         console.log('Error in finding posts ',err);
+    //         return res.redirect('back');
+    //     }
+    //     return res.render('home',{title:'home',posts: posts});
+    // })
 
-module.exports.index = (req,res)=>{
-    return res.render('home',{title:'home'});
+   try {
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user');
+    return res.render('home',{title:'home',posts: posts});
+}catch(err){
+    console.log('Error in finding posts ',err);
+    return res.redirect('back');
+}
+
+    
 }
