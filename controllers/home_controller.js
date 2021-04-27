f2fa460528efee0ba10f800d151021928fba8b8a
Pushpendra kumar <pushpendra466@gmail.com>
2021-04-27 22:29:30 +0530

    changed px to vw in home cards added right div with new users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,18 +1,15 @@
 const Post = require('../models/post');
-module.exports.index = async (req,res)=>{
-    // Post.find({},(err,posts)=>{
-    //     if(err){
-    //         console.log('Error in finding posts ',err);
-    //         return res.redirect('back');
-    //     }
-    //     return res.render('home',{title:'home',posts: posts});
-    // })
+const User = require('../models/user');
 
+module.exports.index = async (req,res)=>{
    try {
+    //    All posts soretd by time at which created at
     let posts = await Post.find({})
     .sort('-createdAt')
     .populate('user');
-    return res.render('home',{title:'home',posts: posts});
+    // To get 5 new users
+    let users =await User.find({}).sort('-createdAt').limit(5);
+    return res.render('home',{title:'home',posts: posts, newUsers: users});
 }catch(err){
     console.log('Error in finding posts ',err);
     return res.redirect('back');
